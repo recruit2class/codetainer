db8895b1a46bed8b0936fe4d5320db173d4770ed
Andy Singleton <asing@brightprocess.com>
2020-05-01 14:00:42 +0100

    Another attemot at cors sane configuration

--- http-handlers.go
+++ http-handlers.go
@@ -41,7 +41,15 @@ func RouteIndex(ctx *Context) error {
 
 func RouteApiV1CodetainerTTY(ctx *Context) error {
 	// Temporary CORS configuration
-	ctx.W.Header().Set("Access-Control-Allow-Origin", "https://gaugepro.site")
+	// ctx.W.Header().Set("Access-Control-Allow-Origin", "https://gaugepro.site")
+
+	// CORS configuration
+	if strings.Contains(ctx.R.RemoteAddr, "[::1]") {
+		ctx.W.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+	}
+	if strings.Contains(ctx.R.RemoteAddr, "gaugepro.site") {
+		ctx.W.Header().Set("Access-Control-Allow-Origin", "https://gaugepro.site")
+	}
 
 	if ctx.R.Method == "POST" {
 		return RouteApiV1CodetainerUpdateCurrentTTY(ctx)
@@ -160,14 +168,6 @@ func RouteApiV1CodetainerImageCreate(ctx *Context) error {
 //        200: TTYBody
 //
 func RouteApiV1CodetainerUpdateCurrentTTY(ctx *Context) error {
-
-	// CORS configuration
-	if strings.Contains(ctx.R.RemoteAddr, "[::1]") {
-		ctx.W.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
-	}
-	if strings.Contains(ctx.R.RemoteAddr, "gaugepro.site") {
-		ctx.W.Header().Set("Access-Control-Allow-Origin", "https://gaugepro.site")
-	}
 
 	vars := mux.Vars(ctx.R)
 	id := vars["id"]
